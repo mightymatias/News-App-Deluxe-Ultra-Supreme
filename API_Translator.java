9d246178b3b49e7caffe8e635d39ad4d139b5708
Austin Matias <austinjmatias@gmail.com>
2021-03-22 17:29:45 -0400

    Adding API connecting functionality, as well as a test of it working to the main method.

--- API_Translator.java
+++ API_Translator.java
@@ -1,2 +1,54 @@
+/*
+Last update: 22 March 2021
+
+Methods that interact directly with the News API.
+
+Contributing authors: Austin Matias
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import org.json.*;
+
+
 public class API_Translator {
+
+    private final String API_KEY = "061df24bf9374cc9a4d897e1655b7aee";
+
+    private final String BASE_URL = "https://newsapi.org/v2/";
+
+    public StringBuilder connectAndReturnContents (String _urlString){
+        try{
+            //Make the connection.
+            URL url = new URL(_urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            //Examine the response code.
+            int status = con.getResponseCode();
+            if (status != 200){
+                return new StringBuilder("Error: API request failed." +
+                        " Status: ").append(status);
+            } else {
+                //Parse input stream into a text string.
+                BufferedReader in = new BufferedReader(new
+                        InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null){
+                    content.append(inputLine);
+                }
+                //Close the connection.
+                in.close();
+                con.disconnect();
+                return content;
+            }
+        } catch (Exception e){
+            return new StringBuilder("Error: ").append(e);
+        }
+
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+/*
+Last update: 22 March 2021
+
+The main method of the project
+
+Contributing authors: Austin Matias
+ */
+
 public class Main {
     public static void main(String[] args) {
 
+        API_Translator api_translator = new API_Translator();
+        String testString = "https://newsapi.org/v2/everything?q=Apple&from=2021-03-22&sortBy=popularity&apiKey=061df24bf9374cc9a4d897e1655b7aee";
+        System.out.println(api_translator.connectAndReturnContents(testString));
     }
 }
