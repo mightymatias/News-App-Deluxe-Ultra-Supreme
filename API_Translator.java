55e8c3a8378da8bc8a8b515033fa3d5c5de21a26
BurnedBacon <oriolus1211@gmail.com>
2021-04-23 18:22:25 -0400

    Storage - Main - Api Interaction Take a look at the text file for easier understand of the storage

--- API_Translator.java
+++ API_Translator.java
@@ -12,25 +12,21 @@ import java.net.*;
 import java.util.ArrayList;
 import org.json.*;
 
-
 public class API_Translator {
 
-    //The API key for the NewsAPI.
-    private final String apiKey = "061df24bf9374cc9a4d897e1655b7aee";
+    private final String API_KEY = "061df24bf9374cc9a4d897e1655b7aee";
 
-    //The prefix used for all API calls.
-    private final String apiPrefix = "&apiKey=";
+    private final String API_PREFIX = "&apiKey=";
 
-    //The base url which is used for all calls.
-    private final String baseUrl = "https://newsapi.org/v2/";
+    private final String BASE_URL = "https://newsapi.org/v2/";
 
-    /**
+    /** Start of API connection
      * A method that connects to the API using a given URL, and returns a string containing data from the given URL.
      * @param _urlString The URL to connect to.
      * @return A StringBuilder object containing the results of the URL connection.
      */
-    private StringBuilder connectAndReturnContents (String _urlString){
-        try{
+    public StringBuilder connectAndReturnContents(String _urlString) {
+        try {
             //Make the connection.
             URL url = new URL(_urlString);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -38,7 +34,7 @@ public class API_Translator {
 
             //Examine the response code.
             int status = con.getResponseCode();
-            if (status != 200){
+            if (status != 200) {
                 System.out.println("connection request failed");
                 return new StringBuilder("Error: API request failed." +
                         " Status: ").append(status);
@@ -47,7 +43,7 @@ public class API_Translator {
                 BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 String inputLine;
                 StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null){
+                while ((inputLine = in.readLine()) != null) {
                     content.append(inputLine);
                 }
                 //Close the connection.
@@ -55,95 +51,130 @@ public class API_Translator {
                 con.disconnect();
                 return content;
             }
-        } catch (Exception e){
-            return new StringBuilder("*****Error: ").append(e);
+        } catch (Exception e) {
+            return new StringBuilder("Error: ").append(e);
         }
 
     }
+    /**END of API connection*/
 
-    /**
+    /**Start of Searching methods
      * Method that retrieves the top headlines for a certain country.
+     *
      * @param _country The country to get the top headlines for.
      * @return A JSON Object containing all the top headlines for a certain country.
      */
-    protected JSONObject getAllTopHeadlinesForCountry(String _country){
+
+    /*  Matias Search by country
+     *   Supported 54 countries
+     * */
+    public JSONObject sortByCountry(String _country) {
         String cleanCountry = _country.toLowerCase();
         String callAction = "top-headlines?country=";
-        String urlString = baseUrl + callAction + cleanCountry + apiPrefix + apiKey;
+        String urlString = BASE_URL + callAction + cleanCountry + API_PREFIX + API_KEY;
 
         StringBuilder content = connectAndReturnContents(urlString);
 
-        try{
+        try {
             return new JSONObject(content.toString());
 
         } catch (Exception e) {
-            System.out.println("****Error: " + e.toString());
+            System.out.println("Error: " + e.toString());
 
         }
         return new JSONObject();
     }
 
-    /**
-     * This class gets the top headlines for a particular category.
-     * @param _category the category of headlines to get.
-     * @return a JSON object containing the top headlines for that category.
-     */
-    protected JSONObject getAllTopHeadlinesForCategory(String _category){
+    /*  Matias Search by category
+     *   Supported possible categories are business, entertainment, general, health, science, sports, technology.
+     * */
+    public JSONObject sortByCategory(String _category) {
         String cleanCategory = _category.toLowerCase();
         String callAction = "top-headlines?country=us&category=";
-        String urlString = baseUrl + callAction + cleanCategory + apiPrefix + apiKey;
+        String urlString = BASE_URL + callAction + cleanCategory + API_PREFIX + API_KEY;
 
         StringBuilder content = connectAndReturnContents(urlString);
 
-        try{
+        try {
             return new JSONObject(content.toString());
 
         } catch (Exception e) {
-            System.out.println("***Error: " + e);
+            System.out.println("Error: " + e);
         }
 
         return new JSONObject();
     }
 
-    /**
-     * This class takes a JSON Object with multiple articles, and pulls a specific one out.
+    /*  Quan Dinh Search by keyword
+     *   Supported single keyword and multiple keywords
+     * */
+    public JSONObject sortByKeyword(String _keyword) {
+        String cleanKw = _keyword.replaceAll("\\s", "_").toLowerCase();
+        String callAction = "top-headlines?q=";
+        String urlString = BASE_URL + callAction + cleanKw + API_PREFIX + API_KEY;
+
+        StringBuilder content = connectAndReturnContents(urlString);
+
+        try {
+            return new JSONObject(content.toString());
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        return new JSONObject();
+    }
+
+    /* Quan Dinh Search by domain
+     *   NOTE: the domain is the name part only, example: www.facebook.com the input will be facebook
+     * */
+    public JSONObject sortByDom(String _domainName) {
+        String cleanDom = _domainName.toLowerCase();
+        String callAction = "top-headlines?domains=";
+        String urlString = BASE_URL + callAction + cleanDom + ".com" + API_PREFIX + API_KEY;
+
+        StringBuilder content = connectAndReturnContents(urlString);
+
+        try {
+            return new JSONObject(content.toString());
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        return new JSONObject();
+    }
+
+    /**END OF Searching methods*/
+
+    /**Start of JSON extraction*
+     * These modthod take a JSON Object with multiple articles, and pulls a specific one out.
      * @param _object The JSON Object containing multiple articles.
-     * @param _articleNumber The index in the array of the article to be pulled out.
      * @return An article object containing information about the article.
      */
-    private Article getSpecificArticleFromJSON(JSONObject _object, int _articleNumber){
+
+    public Article getSpecificArticleFromJSON(JSONObject _object, int _articleNumber){
         try {
             return new Article(_object.getJSONArray("articles").getJSONObject(_articleNumber));
         } catch (Exception e){
-            System.out.println("**Error: " + e);
+            System.out.println("Error: " + e);
         }
         return new Article();
     }
 
-    /**
-     * This method takes in a JSON Object and creates an ArrayList of Article objects.
-     * @param _object the JSON Object containing articles.
-     * @return The ArrayList of article objects.
-     */
-    protected ArrayList<Article> getArrayListOfArticlesFromJSONObject(JSONObject _object){
+    public ArrayList<Article> getArrayListOfArticlesFromJSONObject(JSONObject _object){
         ArrayList<Article> articleArrayList = new ArrayList<>();
         try {
-            for (int i = 0; i < getLengthOfJSONObjectArticleArray(_object); i++){
+            for (int i = 1; i <= _object.getJSONArray("articles").length(); i++){
                 articleArrayList.add(getSpecificArticleFromJSON(_object, i));
             }
         } catch (Exception e){
-            System.out.println("*Error: " + e);
+            System.out.println("Error: " + e);
         }
         return articleArrayList;
     }
 
-    /**
-     * A helper method to get the number of articles in a JSON Object, useful when iterating through the JSON Object
-     * in other methods.
-     * @param _object The JSON Object to get the length of.
-     * @return The number of articles in the JSON Object.
-     */
-    private int getLengthOfJSONObjectArticleArray(JSONObject _object){
+    public int getLengthOfJSONObjectArticleArray(JSONObject _object){
         int length = 0;
         try {
             length = _object.getJSONArray("articles").length();
@@ -152,5 +183,5 @@ public class API_Translator {
         }
         return length;
     }
-
+    /**END of Json extractions*/
 }
--- Storage.java
+++ Storage.java
@@ -1,26 +1,40 @@
 import java.io.*;
+import java.lang.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Storage extends Article {
 
+    /**         HOW DOES THIS CLASS WORK? (BASIC)
+     *          First: Having an ArrayList of favorite Article and Check for existent of txt file (if not exist then create) (if exist then load data into an arraylist)
+     *          Second: (optional) to add an article to favorite                    use method  addArticle (Article _article)
+     *          Third:  (optional) to delete an article from the List               use method  deleteArticle (String _title)
+     *          Fourth: (optional) to select an article from the List               use method  selectArticle (String _title)
+     */
+
     private String filePath = "FavoriteArticles.txt";
     protected ArrayList<Article> favoriteArray = new ArrayList<>();
-    protected int arrayCounter=0;
+
+    /**Default Constructor*/
     public Storage() {
 
     }
 
+    /**Initialization Phase*/
     /**
-     * To be used on program load to ensure that the favorite article file exists, and to create it if it does not,
-     * then to load the array of favorited articles from system storage.
+     * Create file and load file data into array
      */
-    protected void initializeStorage() {
+    protected void initializeStorage() throws IOException {
         ensureFileExistence();
         loadArray();
-
+        clearStorage();
     }
 
+    /**
+     * Check file existence if not -> create the file
+     */
     private boolean ensureFileExistence() {
         //create the file object
         File favoriteArticles = new File(filePath);
@@ -37,6 +51,9 @@ public class Storage extends Article {
         return favoriteArticles.exists();
     }
 
+    /**
+     * Load file data to array
+     */
     private void loadArray() {
         try {
             Scanner input = new Scanner(new File(filePath));
@@ -57,69 +74,96 @@ public class Storage extends Article {
             System.out.println("Error: " + e);
             e.printStackTrace();
         }
-        System.out.println(favoriteArray);
+        //System.out.println(favoriteArray);
     }
-    public void newFavorite(Article article){
-        String title = article.getTitle();
-        String author = article.getAuthor();
-        String description = article.getDescription();
-        String url = article.getUrl();
-        String urlToImage = article.getUrlToImage();
-        String publishedAt = article.getPublishedAt();
-        favoriteArray.add(article);
+
+    /**Modification Phase*/
+    /**Clear storage*/
+    public void clearStorage() throws IOException {
+        FileWriter fw = new FileWriter("FavoriteArticles.txt", false);
+        fw.write("");
+        fw.close();
     }
-    public void storeTxt(String _newFavoriteFile) {
-        try {
-            FileWriter out = new FileWriter(filePath, true);
-            out.append(_newFavoriteFile);
-            out.close();
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-            e.printStackTrace();
+
+    /** Delete from DB
+     /** @paramm _title might be getting from Article.getTitle
+     * Using hashmap to search for that article index in the array
+     * After that remove it from running Array before update storage*/
+    public void deleteArticle (String _title) {
+        /**String gonna be the title as key, Integer gonna be the index of that title in array.*/
+        /** def loadfactor 0.7f*/
+        Map<String, Integer> articleIndex = new HashMap<String, Integer>();
+
+        for (int i = 0; i < favoriteArray.size(); i++) {                                        /** looping any indexing title of article in favoriteArray*/
+            String currentIndexTitle = favoriteArray.get(i).getTitle();
+            int indexValue = i;
+            articleIndex.put(currentIndexTitle, indexValue);                                    /**put current pair ("title", index) into the map*/
+        }
+
+        if (articleIndex.containsKey(_title)) {                                                 /**checking existence of article*/
+            int getIndex = articleIndex.get(_title);                                            /** getting the index from given title*/
+            favoriteArray.remove(getIndex);
+            System.out.println("Article has been removed from your favorite list.");
+        }
+        else
+        {
+            System.out.println("The article you are looking for is not exist!");
         }
     }
 
-    //x will be the input the user enters on the gui.
-    private void add(){
-
-    }
-    private void delete(int x) {
-        try {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter a positive number");
-            if (x < 0) {
-                System.out.println("Error number is negative: ");
-
-            } else if (x > favoriteArray.toArray().length) {
-                System.out.println("Error number is bigger than users list: ");
-            } else {
-                favoriteArray.remove(x);
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-            e.printStackTrace();
+    /** Clear the current txt file and reload into it with new list from the arraylist*/
+    public void updateStorage() throws IOException {
+        clearStorage();
+        FileWriter textLoader = new FileWriter("FavoriteArticles.txt", true);
+        for (int i  = 0; i < favoriteArray.size(); i++) {                                       /**toString each article in the favoriteArray then add it into the DB*/
+            String theArticle = favoriteArray.get(i).toString();
+            textLoader.append(theArticle);
         }
-        saveArrayToFile(favoriteArray);
+        textLoader.close();
     }
 
-    private void clear() {
-        Scanner input = null;
-        try {
-            input = new Scanner(new File(filePath));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        while (input.hasNextLine()) {
-            filePath.format(" ");
-        }
+    /** Add a new article to theDataArray (line 11) but this time require an Article Obj*/
+    public void addArticle(Article _article)
+    {
+        favoriteArray.add(_article);
     }
-    private void saveArrayToFile(ArrayList favoriteArray){
-        for(int i=0;i<favoriteArray.toArray().length;i++){
-            clear();
-            storeTxt(favoriteArray.get(i).toString());
-        }
 
+    /**Support method*/
+    /**Select article from storage
+     * Require title name String
+     * Return article String*/
+    public String selectArticle(String _title) {
+        String theStrArticle  = "";
+        /**String gonna be the title as key, Integer gonna be the index of that title in array.
+         * def loadfactor 0.7f*/
+        Map<String, Integer> articleIndex = new HashMap<String, Integer>();
+
+        for (int i = 0; i < favoriteArray.size(); i++) {                                                                //looping any indexing title of article in favoriteArray
+            String currentIndexTitle = favoriteArray.get(i).getTitle();
+            articleIndex.put(currentIndexTitle, i);                                                                     //put current pair ("title", index) into the map
+        }
+        if (articleIndex.containsKey(_title)) {                                                                         //checking existence of article
+            int getIndex = articleIndex.get(_title);                                                                    //getting the index from given title
+            System.out.println("Article found!");
+            theStrArticle = favoriteArray.get(getIndex).toString();
+            System.out.println(theStrArticle);
+        }
+        else
+        {
+            System.out.println("The article you are looking for is not exist!");
+        }
+        return theStrArticle;
     }
+
+    /**View saved favorite articles*/
+    public void viewArticles() {
+        for (int i = 0; i < favoriteArray.size(); i++)
+        {
+            System.out.println("Article #" + i + ":\n" + favoriteArray.get(i));
+        }
+    }
+
+
 
 }
 
