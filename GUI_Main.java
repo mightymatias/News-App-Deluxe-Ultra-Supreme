00c239922e5c17d6920f72aaf58237d9f05ca2a3
Austin Matias <austinjmatias@gmail.com>
2021-04-30 20:41:53 -0400

    Adding Documentation

--- GUI_Main.java
+++ GUI_Main.java
@@ -56,24 +56,24 @@ public class GUI_Main extends Application {
 
     //List of possible country codes that API takes as input
     private final static String[] countries = new String[] {
-            "ar", "au", "at", "be", "br", "bg", "ca", "cn", "co",
-            "cu", "cz", "eg", "fr", "de", "gr", "hk", "hu", "in",
+            "ar", "au", "at", "be", "br", "bg", "ca", "co",
+            "cu", "cz", "eg", "fr", "de", "gr", "hu", "in",
             "id", "ie", "il", "it", "jp", "lv", "lt", "my", "mx",
             "ma", "nl", "nz", "ng", "no", "ph", "pl", "pt", "ro",
             "ru", "sa", "rs", "sg", "sk", "si", "za", "kr", "se",
-            "ch", "tw", "th", "tr", "ae", "ua", "gb", "us", "ve"
+            "ch", "tw", "tr", "ua", "gb", "us", "ve"
     };
 
     //Full country names associated with the country code
     private final static String[] fullCountryName = new String[] {
             "Argentina", "Australia", "Austria", "Belgium", "Brazil",
-            "Bulgaria", "Canada", "China", "Colombia", "Cuba", "Czech Republic",
-            "Egypt", "France", "Germany", "Greece", "Hong Kong", "Hungary", "India",
+            "Bulgaria", "Canada", "Colombia", "Cuba", "Czech Republic",
+            "Egypt", "France", "Germany", "Greece", "Hungary", "India",
             "Indonesia", "Ireland", "Israel", "Italy", "Japan", "Latvia", "Lithuania",
             "Malaysia", "Mexico", "Morocco", "Netherlands", "New Zealand", "Nigeria",
             "Norway", "Philippines", "Poland", "Portugal", "Romania", "Russia", "Saudi Arabia",
             "Serbia", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea",
-            "Sweden", "Switzerland", "Taiwan", "Thailand", "Turkey", "UAE", "Ukraine",
+            "Sweden", "Switzerland", "Taiwan", "Turkey", "Ukraine",
             "United Kingdom", "United States", "Venezuela"
     };
 
